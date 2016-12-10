7bd85b60261c1aca651bd8ba53a66320a90e2954
Sergey Chikuyonok <serge.che@gmail.com>
2016-12-10 23:09:26 +0300

    Removed output profile code in favour of separate package

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 
-import defaults from './lib/defaults';
-import Profile from './lib/profile';
-
 /**
  * Outputs given parsed abbreviation as HTML
  * @param {Node} tree Parsed abbreviation tree
- * @param {Object} Output options
+ * @param {Profile} Output profile (@see @emmetio/output-profile)
  * @return {String}
  */
-export default function outputHTML(tree, options) {
-    options = Object.assign({}, defaults, options);
+export default function outputHTML(tree, profile) {
+    const profile = new Profile(options);
+
+
+
 };
