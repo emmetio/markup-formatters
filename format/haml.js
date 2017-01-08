45d84a3b3cb1cf22909e420eafd4dd0f3190fbd0
Sergey Chikuyonok <serge.che@gmail.com>
2017-01-08 20:46:30 +0300

    👏 HAML formatter

--- format/haml.js
+++ format/haml.js
@@ -1,12 +1,10 @@
 'use strict';
 
 import render from '../lib/render';
-import { getIndentLevel, formatAttributes } from './assets/indent-formats';
-import { splitByLines, handlePseudoSnippet, isFirstChild, isRoot } from '../lib/utils';
+import indentFormat from './assets/indent-format';
+import { splitByLines, handlePseudoSnippet } from '../lib/utils';
 
-const reOmitName = /^%div$/i;
 const reNl = /\n|\r/;
-const hasPrimaryAttrs = /^[.#]/;
 
 /**
  * Renders given parsed Emmet abbreviation as HAML, formatted according to
@@ -18,32 +16,30 @@ const hasPrimaryAttrs = /^[.#]/;
  */
 export default function haml(tree, profile, options) {
 	options = options || {};
-	const attrOptions = {
-		secondary(attrs) {
-			const str = attrs.map(attr => attr.isBoolean ? attr.name : `${attr.name}=${profile.quote(attr.value)}`).join(' ');
-			return str ? `(${str})` : '';
+	const nodeOptions = {
+		open: '[%NAME][PRIMARY_ATTRS][(SECONDARY_ATTRS)][SELF_CLOSE]',
+		selfClose: '/',
+		attributes: {
+			secondary(attrs) {
+				return attrs.map(attr => attr.isBoolean
+					? `${attr.name}${profile.get('compactBooleanAttributes') ? '' : '=true'}`
+					: `${attr.name}=${profile.quote(attr.value)}`
+				).join(' ');
+			}
 		}
 	};
 
-	return render(tree, options.field, (outNode, renderFields) => {
-		outNode = setFormatting(outNode, profile);
+	return render(tree, options.field, outNode => {
+		outNode = indentFormat(outNode, profile, nodeOptions);
+		outNode = updateFormatting(outNode, profile);
 
-		if (!handlePseudoSnippet(outNode, renderFields)) {
+		if (!handlePseudoSnippet(outNode)) {
 			const node = outNode.node;
-
-			if (node.name) {
-                const name = profile.name(node.name);
-				const attrs = formatAttributes(node, profile, renderFields, attrOptions);
-				// omit tag name if node has primary attributes only
-				const canOmitName = attrs && hasPrimaryAttrs.test(attrs) && reOmitName.test(name);
-
-				outNode.open = (canOmitName ? '' : name) + attrs + (node.selfClosing ? '/' : '');
-			}
 
 			// Do not generate fields for nodes with empty value and children
 			// or if node is self-closed
 			if (node.value || (!node.children.length && !node.selfClosing) ) {
-				outNode.text = renderFields(formatNodeValue(node, profile));
+				outNode.text = outNode.renderFields(formatNodeValue(node, profile));
 			}
 		}
 
@@ -53,44 +49,47 @@ export default function haml(tree, profile, options) {
 
 /**
  * Updates formatting properties for given output node
- * NB Unlike HTML, Pug is indent-based format so some formatting options from
+ * NB Unlike HTML, HAML is indent-based format so some formatting options from
  * `profile` will not take effect, otherwise output will be broken
- * @param  {OutputNode} outNode Output wrapper of farsed abbreviation node
+ * @param  {OutputNode} outNode Output wrapper of parsed abbreviation node
  * @param  {Profile}    profile Output profile
  * @return {OutputNode}
  */
-function setFormatting(outNode, profile) {
+function updateFormatting(outNode, profile) {
 	const node = outNode.node;
-
-    outNode.indent = profile.indent(getIndentLevel(node, profile));
-    outNode.newline = '\n';
-    const prefix = outNode.newline + outNode.indent;
-
-    // do not format the very first node in output
-    if (!isRoot(node.parent) || !isFirstChild(node)) {
-        outNode.beforeOpen = prefix;
-    }
 
     if (!node.isTextOnly && node.value) {
         // node with text: put a space before single-line text
-        outNode.beforeText = reNl.test(node.value) ? prefix + profile.indent(1) : ' ';
+        outNode.beforeText = reNl.test(node.value)
+			? outNode.newline + outNode.indent + profile.indent(1)
+			: ' ';
     }
 
 	return outNode;
 }
-
 /**
- * Formats value of given node: for multiline text we should precede each
- * line with `| ` with one-level deep indent
- * @param  {Node} node
+ * Formats value of given node: for multiline text we should add a ` |` suffix
+ * at the end of each line. Also ensure that text is perfectly aligned.
+ * @param  {Node}    node
  * @param  {Profile} profile
  * @return {String|null}
  */
 function formatNodeValue(node, profile) {
 	if (node.value != null && reNl.test(node.value)) {
+		const lines = splitByLines(node.value);
 		const indent = profile.indent(1);
-		return splitByLines(node.value).map(line => `${indent}| ${line}`).join('\n');
+		const maxLength = lines.reduce((prev, line) => Math.max(prev, line.length), 0);
+
+		return lines.map((line, i) => `${i ? indent : ''}${pad(line, maxLength)} |`).join('\n');
 	}
 
 	return node.value;
+}
+
+function pad(text, len) {
+	while (text.length < len) {
+		text += ' ';
+	}
+
+	return text;
 }
